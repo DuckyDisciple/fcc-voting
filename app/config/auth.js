b2d9ea181b2b220e1b9a3ab6842b9c6ae1b47bed
duckydisciple <duckydesign@gmail.com>
2016-01-29 05:28:23 +0000

    Basic changes and index.html created

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = {
-    'gitHubAuth': {
-        'clientID': process.env.GITHUB_KEY,
-        'clientSecret': process.env.GITHUB_SECRET,
-        'callbackURL': process.env.APP_URL + 'auth/github/callback'
-    },
+    // 'gitHubAuth': {
+    //     'clientID': process.env.GITHUB_KEY,
+    //     'clientSecret': process.env.GITHUB_SECRET,
+    //     'callbackURL': process.env.APP_URL + 'auth/github/callback'
+    // },
     'googleAuth': {
         'clientID': process.env.GOOGLE_KEY,
         'clientSecret': process.env.GOOGLE_SECRET,
--- app/config/passport.js
+++ app/config/passport.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var GitHubStrategy = require('passport-github').Strategy;
+// var GitHubStrategy = require('passport-github').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require("../models/users.js");
 var configAuth = require("./auth.js");
@@ -16,35 +16,35 @@ module.exports = function(passport){
         });
     });
     
-    passport.use(new GitHubStrategy({
-        clientID: configAuth.gitHubAuth.clientID,
-        clientSecret: configAuth.gitHubAuth.clientSecret,
-        callbackURL: configAuth.gitHubAuth.callbackURL
-    },
-    function(token,refreshToken,profile,done){
-        process.nextTick(function(){
-            User.findOne({'github.id':profile.id},function(err,user){
-                if(err) return done(err);
-                if(user){
-                    return done(null, user);
-                }else{
-                    var newUser = new User();
+    // passport.use(new GitHubStrategy({
+    //     clientID: configAuth.gitHubAuth.clientID,
+    //     clientSecret: configAuth.gitHubAuth.clientSecret,
+    //     callbackURL: configAuth.gitHubAuth.callbackURL
+    // },
+    // function(token,refreshToken,profile,done){
+    //     process.nextTick(function(){
+    //         User.findOne({'github.id':profile.id},function(err,user){
+    //             if(err) return done(err);
+    //             if(user){
+    //                 return done(null, user);
+    //             }else{
+    //                 var newUser = new User();
                     
-                    newUser.github.id = profile.id;
-                    newUser.github.displayName = profile.displayName;
-                    newUser.github.username = profile.username;
-                    newUser.github.publicRepos = profile._json.public_repos;
-                    newUser.numClicks.clicks = 0;
+    //                 newUser.github.id = profile.id;
+    //                 newUser.github.displayName = profile.displayName;
+    //                 newUser.github.username = profile.username;
+    //                 newUser.github.publicRepos = profile._json.public_repos;
+    //                 newUser.numClicks.clicks = 0;
                     
-                    newUser.save(function(err){
-                        if(err) throw err;
+    //                 newUser.save(function(err){
+    //                     if(err) throw err;
                         
-                        return done(null, newUser);
-                    });
-                }
-            });
-        });
-    }));
+    //                     return done(null, newUser);
+    //                 });
+    //             }
+    //         });
+    //     });
+    // }));
     
     passport.use(new GoogleStrategy({
         clientID: configAuth.googleAuth.clientID,
