4804dda69f9c32c38b4a9752d726e36512466a38
duckydisciple <duckydesign@gmail.com>
2016-02-03 15:42:10 +0000

    Edit page form validation

--- client/js/pollEditFunctions.js
+++ client/js/pollEditFunctions.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
     $(".locked").prop("readOnly",true);
+    $(".submit").prop("disabled",true);
 });
+
+$(document).on("keyup","input",checkInputs);
+
+function checkInputs(){
+    var allGood = true;
+    $("input").each(function(){
+        if($(this).val().length===0) allGood=false;
+    });
+    
+    if(allGood){
+        $(".submit").prop("disabled",false);
+    }else{
+        $(".submit").prop("disabled",true);
+    }
+}
 
 $(".add-opt").click(function(){
     var newIndex = document.getElementsByClassName("opt").length;
@@ -11,4 +27,5 @@ $(".add-opt").click(function(){
     newOption.setAttribute("name","opt"+newIndex);
     optionsDiv.appendChild(newOption);
     optionsDiv.appendChild(document.createElement("BR"));
+    checkInputs();
 });
